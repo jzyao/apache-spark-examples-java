b58bf9ee592833ae4d1f03dd648aa4b7dbcd654f
Adrian Novegil <adrian.novegil@gmail.com>
2014-12-08 17:03:10 +0100

    Se ha añadido un ejemplo de contador de palabras en Java.

---
+++ apache-spark-wordcount-java/src/main/java/es/devcircus/sparkwordcount/JavaWordCount.java
@@ -0,0 +1,109 @@
+/**
+ * This file is part of apache-spark-examples.
+ *
+ * apache-spark-examples is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2, or (at your option) any later version.
+ *
+ * apache-spark-examples is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; see the file COPYING. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package es.devcircus.sparkwordcount;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.apache.spark.api.java.*;
+import org.apache.spark.api.java.function.*;
+import org.apache.spark.SparkConf;
+import scala.Tuple2;
+
+/**
+ *
+ * @author Adrian Novegil <adrian.novegil@gmail.com>
+ */
+public class JavaWordCount {
+
+    public static void main(String[] args) {
+
+        JavaSparkContext sc = new JavaSparkContext(new SparkConf().setAppName("Spark Count"));
+
+        final int threshold = Integer.parseInt(args[1]);
+
+        // split each document into words
+        JavaRDD<String> tokenized = sc.textFile(args[0]).flatMap(
+                new FlatMapFunction<String, String>() {
+                    @Override
+                    public Iterable<String> call(String s) {
+                        return Arrays.asList(s.split(" "));
+                    }
+                }
+        );
+
+        // count the occurrence of each word
+        JavaPairRDD<String, Integer> counts = tokenized.mapToPair(
+                new PairFunction<String, String, Integer>() {
+                    @Override
+                    public Tuple2<String, Integer> call(String s) {
+                        return new Tuple2<String, Integer>(s, 1);
+                    }
+                }
+        ).reduceByKey(
+                new Function2<Integer, Integer, Integer>() {
+                    @Override
+                    public Integer call(Integer i1, Integer i2) {
+                        return i1 + i2;
+                    }
+                }
+        );
+
+        // filter out words with less than threshold occurrences
+        JavaPairRDD<String, Integer> filtered = counts.filter(
+                new Function<Tuple2<String, Integer>, Boolean>() {
+                    @Override
+                    public Boolean call(Tuple2<String, Integer> tup) {
+                        return tup._2() >= threshold;
+                    }
+                }
+        );
+
+        // count characters
+        JavaPairRDD<Character, Integer> charCounts = filtered.flatMap(
+                new FlatMapFunction<Tuple2<String, Integer>, Character>() {
+                    @Override
+                    public Iterable<Character> call(Tuple2<String, Integer> s) {
+                        Collection<Character> chars = new ArrayList<Character>(s._1().length());
+                        for (char c : s._1().toCharArray()) {
+                            chars.add(c);
+                        }
+                        return chars;
+                    }
+                }
+        ).mapToPair(
+                new PairFunction<Character, Character, Integer>() {
+                    @Override
+                    public Tuple2<Character, Integer> call(Character c) {
+                        return new Tuple2<Character, Integer>(c, 1);
+                    }
+                }
+        ).reduceByKey(
+                new Function2<Integer, Integer, Integer>() {
+                    @Override
+                    public Integer call(Integer i1, Integer i2) {
+                        return i1 + i2;
+                    }
+                }
+        );
+
+        // print results
+        System.out.println(charCounts.collect());
+
+    }
+}
